65a619411bd37d6745fe627bc4de1024d54ceaed
loundness <ludo.vic@me.com>
2019-04-08 00:02:29 +0200

    home page

---
+++ src/Components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+
+
+class HomePage extends React.Component {
+    render () {
+        return(
+            <div className="Home-page">
+                <h1>Home Page</h1>
+            </div>
+        )
+}
+}
+
+export default HomePage
