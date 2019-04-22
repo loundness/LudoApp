d79247014c9cacc8242d02e6fbeb383b1a94b45e
loundness <ludo.vic@me.com>
2019-04-22 22:55:43 +0200

    jumbotron home page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LudoApp #
+# City App #
 ----------
 
 
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import '../App.css'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Carousel from 'react-bootstrap/Carousel'
 
 
 class HomePage extends React.Component {
-    render () {
-        return(
+    render() {
+        return (
             <div className="Home-page">
-                <img src="../img/homeImage.jpg" width= '100%' min-height='100vh'/>
-                
+                <Jumbotron fluid id="jumbotronHome">
+                <Container>
+                    <h1>City App</h1>
+                    <p>
+                    Bienvenue sur l'application City App pour retrouver des stations de vélos libre.
+                    </p>
+                </Container>
+                </Jumbotron>
             </div>
         )
-}
+    }
 }
 
 export default HomePage
