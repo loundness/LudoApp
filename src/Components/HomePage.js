39975ea88c75d2ff50f762efbd233628257e856e
loundness <ludo.vic@me.com>
2019-04-25 00:00:49 +0200

    rajout d'un carousel

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../App.css'
+import Image from 'react-bootstrap/Image'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Carousel from 'react-bootstrap/Carousel'
@@ -17,7 +18,46 @@ class HomePage extends React.Component {
                     </p>
                 </Container>
                 </Jumbotron>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="carouselHome"
+                        src="../img/velov.jpg"
+                        alt="First slide"
+                        rounded 
+                        />
+                        <Carousel.Caption>
+                        <h3>Premier Slide</h3>
+                        <p>Velov à Lyon</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="carouselHome"
+                        src="../img/velib.jpg"
+                        alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Deuxiemes slide</h3>
+                        <p>Velib à Paris.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="carouselHome"
+                        src="../img/indigo.jpg"
+                        alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Troisieme slide</h3>
+                        <p>Indigo en France.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </div>
+           
         )
     }
 }
